97eab545dd39ebb763b0198498d1248ae5d94c69
pemiinem <pedro.m.rodiguero@gmail.com>
2020-09-19 15:27:56 -0300

    first C program

---
+++ c/exercises/trabalho/missao_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+    float peso, altura;
+    int num;
+
+    printf("--------------------------/n");
+    printf("hello world");
+
+    return 0;
+}
