06fdf0955902e6d6f6c0f42dcfa52df5f021ebfd
pemiinem <pedro.m.rodiguero@gmail.com>
2020-11-25 09:57:02 -0300

    Update on file

---
+++ c/missions/missao9.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu();
+int primo_verify (int x);
+char * regra_mais_quebrada (int regra_triplo, int regra_positivo, int regra_primo);
+// void * listar_numero (int *x[], int qtde, int i);
+int position_editor (int x, int edit_position);
+
+int main(){
+
+    int i, cont = -1, regras_quebradas = 0, soma = 0, qtde = 0, cont_numeros = 0;
+    int x[100], maior = -1, menor = 101, divisor, primo = 0, edit_position;
+    int option, regra_positivo = 0, regra_triplo = 0, regra_primo = 0;
+    float media, result;
+
+    // printf("Quantos numeros deseja cadastrar? ");
+    // scanf("%d", &cont);
+
+    // for (i = 0; i < cont; i++){
+    //     printf("Digite o numero: ");
+    //     scanf("%d", &x[i]);
+        
+    //     while (x[i] < 0 || x[i] > cont * 3 || (primo_verify(x[i]) == 1 && x[i] != 1)){
+    //         printf("Numero invalido!! Tente novamente!! \n");
+    //         regras_quebradas++;
+            
+    //         if (x[i] < 0){
+    //             regra_positivo++;
+    //         } else if (x[i] > cont * 3){
+    //             regra_triplo++;
+    //         } else if (primo_verify(x[i]) == 1 && x[i] != 1){
+    //             regra_primo++;
+    //         }
+
+    //         printf("Digite outro numero: ");
+    //         scanf("%d", &x[i]);
+    //     }   
+    //     if (x[i] > maior){
+    //         maior = x[i];
+    //     } 
+    //     if (x[i] < menor){
+    //          menor = x[i];
+    //     }
+    //     soma = soma + x[i];
+    // }
+
+    do {
+        menu();
+        scanf("%d", &option);
+        switch (option){
+            case 1:                                        //Inserir numeros
+                printf("Quantos numeros deseja cadastrar: ");
+                scanf(" %d", &cont_numeros);
+                for (i = 0; i < cont_numeros; i++){
+                    printf("Digite um numero: ");
+                    scanf(" %d", &x[qtde]);
+                }
+                break;
+
+            case 2:                                        //Listar numeros
+                // listar_numero (x[i], qtde, i);
+                break;
+
+            case 3:                                         //Editar posicao
+                printf("Qual posicao deseja editar? ");
+                scanf("%d", &edit_position);
+                break;
+            
+            case 4:                                         //Excluir posicao 
+                printf("Maior numero digitado: %d \n", maior);
+                break;
+
+            case 5:                                         //Listar numeros maior que media
+                printf("Menor numero digitado: %d \n", menor);
+                break;
+            
+            case 6:                                         //Finalizar o programa
+                printf("Obrigado por participar!! Volte sempre!! \n");
+                break;
+
+            default:
+                printf("Opcao invalida!! Tente novamente!! \n");
+            }
+    } while (option != 6);
+
+    return 0; 
+}
+
+void menu (){
+    printf("|---------------------------------------| \n");
+    printf("|                  MENU                 | \n");
+    printf("|   1 - Inserir numeros                 | \n");
+    printf("|   2 - Listar numeros                  | \n");
+    printf("|   3 - Editar posicao                  | \n");
+    printf("|   4 - Excluir posicao                 | \n");
+    printf("|   5 - Listar numeros maior que media  | \n");
+    printf("|   6 - Sair                            | \n");
+    printf("|---------------------------------------| \n");
+
+    printf("Selecione uma das opcoes: ");
+}
+
+int primo_verify (int x){
+
+    int i, verify = 1;
+
+    for (i = 2; i <= x / 2; i ++){
+        if (x % i == 0){
+            verify = 0;
+            break;
+        }
+    }
+
+    return verify;
+}
+
+char * regra_mais_quebrada (int regra_triplo, int regra_positivo, int regra_primo){
+
+    printf("%d \n", regra_positivo);
+    printf("%d \n", regra_triplo);
+    printf("%d \n", regra_primo);
+    if (regra_triplo > regra_positivo && regra_triplo > regra_primo){
+        return "Regra triplo de N";
+    } else if (regra_positivo > regra_triplo && regra_positivo > regra_triplo){
+        return "Regra positivo";
+    } else if (regra_primo > regra_triplo && regra_primo > regra_positivo){
+        return "Regra do numero primo";
+    }
+}
+
+// void * listar_numero (int *x[], int qtde, int i){
+
+//     for (i = 0; i < qtde; i++){
+//         printf("Numero: %d", x[i]);
+//     }
+// }
+
+int position_editor (int x, int edit_position){
+
+    return x;
+}
+
